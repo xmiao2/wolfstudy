c6737a7925663f2fc1f71ce1552c59e40cd900b7
jmreesjo <jmreesjo@ncsu.edu>
2016-01-12 23:35:27 -0500

    Added question page database retrieval

--- wolfstudy/app.py
+++ wolfstudy/app.py
@@ -26,13 +26,33 @@ def init_db():
 
 @app.before_request
 def before_request():
+    # Open a connection to the database and store it in g.
     g.db = connect_db()
 
 @app.teardown_request
 def teardown_request(exception):
+    # Close the database connection.
     db = getattr(g, 'db', None)
     if db is not None:
         db.close()
+
+def db_add_question(title, content):
+    """Add a question with the specified title and content to the database."""
+    cursor = g.db.cursor()
+
+    cursor.execute('INSERT INTO questions (title, content) VALUES (?, ?)', [title, content])
+    cursor.execute('SELECT last_insert_rowid()')
+
+    new_question_id = cursor.fetchone()[0]
+    g.db.commit()
+
+    return new_question_id
+
+def db_get_question(question_id):
+    """Retrieve and return a question with the specified question id from the database."""
+    cursor = g.db.cursor()
+    cursor.execute('SELECT title, content FROM questions WHERE id == ?', [question_id])
+    return cursor.fetchone()
 
 # +----------+
 # |  ROUTES  |
@@ -51,7 +71,8 @@ def feed():
 
 @app.route('/question/<int:question_id>/')
 def get_question(question_id):
-    return render_template('question.html')
+    title, content = db_get_question(question_id)
+    return render_template('question.html', title=title, content=content)
 
 @app.route('/question/<int:question_id>/answer/', methods=['POST'])
 def answer_question(question_id):
@@ -61,13 +82,9 @@ def answer_question(question_id):
 def ask_question():
     if request.method == 'GET':
         return render_template('ask.html')
+
     elif request.method == 'POST':
-        cursor = g.db.cursor()
-        cursor.execute('INSERT INTO questions (title, content) VALUES (?, ?)',
-                     [request.form['title'], request.form['content']])
-        cursor.execute('SELECT last_insert_rowid()')
-        new_question_id = cursor.fetchone()[0]
-        g.db.commit()
+        new_question_id = db_add_question(request.form['title'], request.form['content'])
         return redirect(url_for('get_question', question_id=new_question_id))
 
 if __name__ == '__main__':
