a3ea1ee066408c25dd4761c3445c494e218d3a1c
jmreesjo <jmreesjo@ncsu.edu>
2016-01-16 23:56:18 -0500

    Adds functionality for answering questions

--- wolfstudy/app.py
+++ wolfstudy/app.py
@@ -61,6 +61,20 @@ def db_get_all_questions():
     questions = [{'title': row[0], 'content': row[1], 'id': row[2]} for row in cursor.fetchall()]
     return questions
 
+def db_add_answer(question_id, content):
+    """Add an answer to the database with an associated question id and text content."""
+    cursor = g.db.cursor()
+    cursor.execute('INSERT INTO answers (question_id, content) VALUES (?, ?)', [question_id, content])
+    g.db.commit()
+
+def db_get_answers(question_id):
+    """Return a list of answers associated with the specified question id."""
+    cursor = g.db.cursor()
+    cursor.execute('SELECT (content) FROM answers WHERE question_id == ?', [question_id])
+    answers = [answer[0] for answer in cursor.fetchall()]
+    print(answers)
+    return answers
+
 # +----------+
 # |  ROUTES  |
 # +----------+
@@ -75,16 +89,20 @@ def db_get_all_questions():
 @app.route('/feed/')
 def feed():
     questions = db_get_all_questions()
-    return render_template('feed.html', questions = questions)
+    return render_template('feed.html', questions=questions)
 
 @app.route('/question/<int:question_id>/')
 def get_question(question_id):
     title, content = db_get_question(question_id)
-    return render_template('question.html', title=title, content=content)
+    answers = db_get_answers(question_id)
+    return render_template('question.html', title=title, content=content, question_id=question_id, answers=answers)
 
 @app.route('/question/<int:question_id>/answer/', methods=['POST'])
 def answer_question(question_id):
-    return 'Posting new answer for question %d.' % question_id
+    db_add_answer(question_id, request.form['content'])
+    return redirect(url_for('get_question', question_id=question_id))
+    #return 'Posting new answer. question_id: %d, content: %s' % (question_id, request.form['content'])
+
 
 @app.route('/ask/', methods=['GET', 'POST'])
 def ask_question():
