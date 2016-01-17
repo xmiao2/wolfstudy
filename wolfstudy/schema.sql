1cdb97065fdee90740a8a58710ea41442ca1c577
jmreesjo <jmreesjo@ncsu.edu>
2016-01-16 23:56:59 -0500

    Changes hard tabs to 4 spaces

--- wolfstudy/schema.sql
+++ wolfstudy/schema.sql
@@ -3,16 +3,16 @@ PRAGMA foreign_keys = on; -- Enforce foreign key constraints
 DROP TABLE IF EXISTS questions;
 CREATE TABLE questions (
     -- id is an alias for ROWID because it is of type INTEGER PRIMARY KEY;
-	-- it auto-increments by default. See SQLite3 documentation.
-	id       INTEGER PRIMARY KEY,
-	title    TEXT NOT NULL,
-	content  TEXT NOT NULL
+    -- it auto-increments by default. See SQLite3 documentation.
+    id       INTEGER PRIMARY KEY,
+    title    TEXT NOT NULL,
+    content  TEXT NOT NULL
 );
 
 DROP TABLE IF EXISTS answers;
 CREATE TABLE answers (
-	id           INTEGER PRIMARY KEY,
-	question_id  INTEGER,
-	content      TEXT NOT NULL,
-	FOREIGN KEY(question_id) REFERENCES questions(id)
+    id           INTEGER PRIMARY KEY,
+    question_id  INTEGER,
+    content      TEXT NOT NULL,
+    FOREIGN KEY(question_id) REFERENCES questions(id)
 );
