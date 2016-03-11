0c48c02174ddd2320e4d60ecd66d30c9e6519d00
jmreesjo <jmreesjo@ncsu.edu>
2016-03-11 14:41:11 -0500

    Add Flask-Script

--- manage.py
+++ manage.py
@@ -1,7 +1,9 @@
 import os
 from wolfstudy import create_app
+from flask.ext.script import Manager
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+manager = Manager(app)
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
