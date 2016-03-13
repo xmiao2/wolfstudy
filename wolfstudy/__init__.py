18a8342c548872a242bc8417bb1a153e0514ca0b
jmreesjo <jmreesjo@ncsu.edu>
2016-03-13 00:02:40 -0500

    Adds Flask-Bootstrap

--- wolfstudy/__init__.py
+++ wolfstudy/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from config import config
+from flask.ext.bootstrap import Bootstrap
 from flask.ext.sqlalchemy import SQLAlchemy
 
 import os
@@ -7,6 +8,7 @@ import sys
 
 
 
+bootstrap = Bootstrap()
 db = SQLAlchemy()
 
 
@@ -28,6 +30,7 @@ def create_app(config_name):
     config[config_name].init_app(app)
     install_secret_key(app)
 
+    bootstrap.init_app(app)
     db.init_app(app)
 
     from .main import main as main_blueprint
