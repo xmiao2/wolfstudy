3468d06acd480607b341118e40bd6c0644fa2cdb
jmreesjo <jmreesjo@ncsu.edu>
2016-03-15 20:46:08 -0400

    Automatically create secret key file with /dev/urandom if it doesn't exist

--- wolfstudy/__init__.py
+++ wolfstudy/__init__.py
@@ -1,8 +1,9 @@
-from flask import Flask
 from config import config
+from flask import Flask
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.sqlalchemy import SQLAlchemy
 
+import errno
 import os
 import sys
 
@@ -14,14 +15,32 @@ db = SQLAlchemy()
 
 
 def install_secret_key(app, filename='secret_key'):
+    """Read the secret key from a file in the application's instance
+    directory. If the directory or file do not exist, create them and
+    write a new secret key from /dev/urandom. We use a file because
+    it maintains existing user sessions across server restarts by using
+    the same secret key.
+    """
     filename = os.path.join(app.instance_path, filename)
+
+    # Make the directory app.instance_path if it does not exist.
+    try:
+        os.makedirs(app.instance_path)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(app.instance_path):
+            pass
+        else:
+            raise
+    
+    # Write 24 random bytes to the file.
+    with open(filename, 'a') as f:
+        f.write(os.urandom(24))
+
+    # Load the secret key file into app.config.
     try:
         app.config['SECRET_KEY'] = open(filename, 'rb').read()
     except IOError:
-        print 'Error: No secret key. Create it with:'
-        if not os.path.isdir(os.path.dirname(filename)):
-            print 'mkdir -p', os.path.dirname(filename)
-        print 'head -c 24 /dev/urandom >', filename
+        print 'Error: Secret key could not be read from %s.' % filename
         sys.exit(1)
 
 def create_app(config_name):
