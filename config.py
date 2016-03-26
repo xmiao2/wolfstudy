d70900971ba5566a4ffa9b7dee68ef3e915f083b
jmreesjo <jmreesjo@ncsu.edu>
2016-03-26 00:29:07 -0400

    Load application environment variables into app's config

--- config.py
+++ config.py
@@ -2,9 +2,13 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    #SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 
     @staticmethod
     def init_app(app):
--- wolfstudy/__init__.py
+++ wolfstudy/__init__.py
@@ -15,40 +15,10 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
-def install_secret_key(app, filename='secret_key'):
-    """Read the secret key from a file in the application's instance
-    directory. If the directory or file do not exist, create them and
-    write a new secret key from /dev/urandom. We use a file because
-    it maintains existing user sessions across server restarts by using
-    the same secret key.
-    """
-    filename = os.path.join(app.instance_path, filename)
-
-    # Make the directory app.instance_path if it does not exist.
-    try:
-        os.makedirs(app.instance_path)
-    except OSError as exc:
-        if exc.errno == errno.EEXIST and os.path.isdir(app.instance_path):
-            pass
-        else:
-            raise
-    
-    # Write 24 random bytes to the file.
-    with open(filename, 'a') as f:
-        f.write(os.urandom(24))
-
-    # Load the secret key file into app.config.
-    try:
-        app.config['SECRET_KEY'] = open(filename, 'rb').read()
-    except IOError:
-        print 'Error: Secret key could not be read from %s.' % filename
-        sys.exit(1)
-
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
-    install_secret_key(app)
 
     bootstrap.init_app(app)
     db.init_app(app)
